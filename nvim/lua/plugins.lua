bd422922404244694c1bc675a49105dfc3c51b80
Igor Pelekhan <dont@bzzr.me>
2022-02-11 20:49:21 +0000

    Add better-escape.nvim plugin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,4 +1,10 @@
 return require('packer').startup(function()
   use 'wbthomason/packer.nvim'                          
   use 'sainnhe/everforest'
+  use {
+	  "max397574/better-escape.nvim",
+	  config = function()
+		  require("better_escape").setup()
+	  end,
+  }
 end)
