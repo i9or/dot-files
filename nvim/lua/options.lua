dbfccd9d722d5223e7a283494006599cc010343d
Igor <dont@bzzr.me>
2022-03-10 20:13:54 +0000

    Update NeoVim configuration

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,3 +1,10 @@
+vim.opt.colorcolumn = '80'
+vim.opt.number = true
+
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.smartindent = true
+
 vim.opt.termguicolors = true
 vim.opt.background = 'dark'
 vim.g['everforest_background'] = 'hard'
